52b987f9da07881b24c7dff182ffcc7448ec6859
flyingice <flyingice.eth@outlook.com>
2022-04-25 21:39:38 +0800

    fix comment in oh-my-zsh config

--- zsh/.config/zsh/oh-my-zsh.zsh
+++ zsh/.config/zsh/oh-my-zsh.zsh
@@ -23,8 +23,8 @@ bindkey '`' autosuggest-accept
 
 
 # Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Standard plugins can be found in ~/.config/oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.config/oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(tmux vi-mode fzf autojump git gpg-agent zsh-autosuggestions zsh-syntax-highlighting)
