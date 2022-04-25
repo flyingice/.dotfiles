15139eabf2c56cafd425ff6e68b3cc5022837394
flyingice <flyingice.eth@outlook.com>
2022-04-25 20:37:16 +0800

    enable vi-mode plugin via oh-my-zsh

--- zsh/.config/zsh/oh-my-zsh.zsh
+++ zsh/.config/zsh/oh-my-zsh.zsh
@@ -12,22 +12,19 @@ ZSH_THEME="bira"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(tmux git fzf autojump zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(tmux vi-mode fzf autojump git zsh-autosuggestions zsh-syntax-highlighting)
 
 # tmux settings
 # don't attach to a tmux session when terminal is launched within IDE
 if [[ $TERMINAL_EMULATOR != "JetBrains-JediTerm" ]]; then
-    export ZSH_TMUX_AUTOSTART=true
+    ZSH_TMUX_AUTOSTART=true
 fi
-export ZSH_TMUX_CONFIG="$XDG_CONFIG_HOME"/tmux/tmux.conf
-export ZSH_TMUX_UNICODE=true
-export ZSH_TMUX_DEFAULT_SESSION_NAME='localhost'
+ZSH_TMUX_CONFIG="$XDG_CONFIG_HOME"/tmux/tmux.conf
+ZSH_TMUX_UNICODE=true
+ZSH_TMUX_DEFAULT_SESSION_NAME='localhost'
 
 # zsh-autosuggestions plugin accept suggestion
 bindkey '`' autosuggest-accept
 
 source $ZSH/oh-my-zsh.sh
 source $ZSH/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# enable vim mode for command line
-bindkey -v
