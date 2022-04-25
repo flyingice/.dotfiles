702e404b3185e9cdce3a154f97b65a47a435321b
flyingice <flyingice.eth@outlook.com>
2022-04-25 21:00:02 +0800

    reorganize init.zsh

--- zsh/.config/zsh/init.zsh
+++ zsh/.config/zsh/init.zsh
@@ -1,6 +1,18 @@
 [[ ${PATH#*$HOME/.local/bin} == $PATH ]] && export PATH=$HOME/.local/bin:$PATH
 
-# extend history settings in ~/.oh-my-zsh/lib/history.zsh
+# ====
+# ==== system-wide default settings
+# ====
+
+export EDITOR=vim
+
+export PAGER=less
+
+# ====
+# ==== history settings
+# ====
+
+# set non-default .zsh_history location
 export HISTFILE="$XDG_CONFIG_HOME"/zsh/zsh-history
 # remove copies in the history list while keeping the newly added one
 setopt HIST_IGNORE_ALL_DUPS
@@ -11,20 +23,27 @@ setopt HIST_NO_STORE
 # don't store function definitions
 setopt HIST_NO_FUNCTIONS
 
-# set system-wide default editor
-export EDITOR=vim
+# ====
+# ==== vim
+# ====
 
-# set system-wide default pager
-export PAGER=less
+# set non-default .vimrc location
+export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
 
-# fzf settings
+# ====
+# ==== command-line fuzzy finder
+# ====
+
+# https://github.com/junegunn/fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS="--height 50% --reverse"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-# vim settings
-export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
+# ====
+# ==== GnuPG
+# ====
 
+# set non-default gpg config location
 export GNUPGHOME="$XDG_CONFIG_HOME"/gnupg
 # Let gpg-agent communicate with ssh-agent so that the auth subkey
 # managed by gnupg can be used during ssh authentification.
