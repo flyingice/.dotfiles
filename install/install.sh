d2c3ff886f0633c28b0859104c0a0c49ba5240be
flyingice <flyingice.eth@outlook.com>
2022-04-28 18:18:03 +0800

    add --override option for stow command in install script

--- install/install.sh
+++ install/install.sh
@@ -372,7 +372,7 @@ deploy_config() {
     # GNU Stow is a symlink farm manager
     # https://www.gnu.org/software/stow/manual/stow.html
     if ! command_exists stow; then install_package 'stow' --force; fi
-    stow --target "$HOME" --dir "$DOTFILE_ROOT" --no-folding "$config" \
+    stow --target "$HOME" --dir "$DOTFILE_ROOT" --override='.*' --no-folding "$config" \
       || fmt_info "Deployment failure: remove your old $config config"
   }
 }
