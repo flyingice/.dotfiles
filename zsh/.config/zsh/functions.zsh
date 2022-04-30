190fab144b6bd86909ebb96b3f8e6485ab20f6fb
flyingice <flyingice.eth@outlook.com>
2022-04-30 13:35:12 +0800

    replace hardcoded keybindings by local variables

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -44,17 +44,20 @@ ideavim_update() {
 # https://github.com/junegunn/fzf/blob/master/ADVANCED.md#switching-between-ripgrep-mode-and-fzf-mode
 # check available actions: https://github.com/junegunn/fzf/blob/master/src/options.go
 # {q} in the reload command evaluates to the query string on fzf prompt
+# alt-f/alt-r to switch between fd and rg mode
 irg() {
     local initial_query="${*:-}"
     local fd_command="$FZF_DEFAULT_COMMAND"
     local rg_command='rg --no-heading --color=always'
+    local fd_key='alt-f'
+    local rg_key='alt-r'
 
     FZF_DEFAULT_COMMAND="$rg_command $(printf %q "$initial_query")" \
     fzf --ansi \
         --disabled --query "$initial_query" \
         --bind "change:reload:sleep 0.1; $rg_command {q} || true" \
-        --bind "alt-f:unbind(change,alt-f)+change-prompt($FD_PROMPT)+enable-search+reload($fd_command)+rebind(alt-r)" \
-        --bind "alt-r:unbind(alt-r)+change-prompt($RG_PROMPT)+disable-search+reload($rg_command {q} || true)+rebind(change,alt-f)" \
+        --bind "${fd_key}:unbind(change,$fd_key)+change-prompt($FD_PROMPT)+enable-search+reload($fd_command)+rebind($rg_key)" \
+        --bind "${rg_key}:unbind($rg_key)+change-prompt($RG_PROMPT)+disable-search+reload($rg_command {q} || true)+rebind(change,$fd_key)" \
         --prompt "$RG_PROMPT" \
         --delimiter : \
         --preview 'line_nb={2}; [[ -z $line_nb ]] && line_nb=1; bat --highlight-line $line_nb --line-range=:100 {1}' \
