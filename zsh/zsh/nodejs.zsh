8fdbdbfc6da9de687c7f3db58b5dcc6f3487e795
flyingice <flyingice.eth@outlook.com>
2022-05-04 22:17:11 +0800

    add nodejs config

---
+++ zsh/zsh/nodejs.zsh
@@ -0,0 +1,6 @@
+export NVM_DIR=$CONFIG_HOME/nvm
+
+# load nvm
+[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && source "/opt/homebrew/opt/nvm/nvm.sh"
+# load nvm bash_completion
+[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
