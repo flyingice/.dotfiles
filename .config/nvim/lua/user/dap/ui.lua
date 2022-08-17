24725aab3a80baa2e6f96d148d030432f75f11ae
flyingice <flyingice.eth@outlook.com>
2022-08-16 22:16:55 -0700

    feat(neovim): deprecate 'sidebar' and 'tray' options

--- .config/nvim/lua/user/dap/ui.lua
+++ .config/nvim/lua/user/dap/ui.lua
@@ -20,7 +20,7 @@ end
 local set = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-set('n','<Leader>dk', "<cmd>lua require('dapui').eval()<CR>", opts)
+set('n', '<Leader>dk', "<cmd>lua require('dapui').eval()<CR>", opts)
 
 --[[
 use nvim-dap events to open and close the window automatically (:help dap-extensions)
@@ -58,28 +58,28 @@ dapui.setup({
   },
   -- expand lines larger than the window
   expand_lines = vim.fn.has('nvim-0.7'),
-  sidebar = {
-    -- change the order of elements in the sidebar
-    elements = {
-      -- provide as ID strings or tables with 'id' and 'size' keys
-      {
-        id = 'scopes',
-        -- can be float or integer > 1
-        size = 0.25,
+  layouts = {
+    {
+      -- change the order of elements in the sidebar
+      elements = {
+        'scopes',
+        'breakpoints',
+        'stacks',
+        'watches',
       },
-      { id = 'breakpoints', size = 0.25 },
-      { id = 'stacks', size = 0.25 },
-      { id = 'watches', size = 00.25 },
+      size = 40,
+      -- can be 'left', 'right', 'top', 'bottom'
+      position = 'left',
     },
-    size = 40,
-    -- can be 'left', 'right', 'top', 'bottom'
-    position = 'left',
-  },
-  tray = {
-    elements = { 'repl' },
-    size = 10,
-    -- can be 'left', 'right', 'top', 'bottom'
-    position = 'bottom',
+    {
+      elements = {
+        'repl',
+        'console',
+      },
+      size = 10,
+      -- can be 'left', 'right', 'top', 'bottom'
+      position = 'bottom',
+    },
   },
   floating = {
     -- can be integers or a float between 0 and 1
