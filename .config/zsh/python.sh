c4d8747e87792f35e20e929347ae6740382e1dd0
flyingice <flyingice.eth@outlook.com>
2026-02-14 11:28:48 -0800

    feat(python): add anaconda config

--- .config/zsh/python.sh
+++ .config/zsh/python.sh
@@ -1,2 +1,17 @@
 # required by pipx
 [[ ${PATH%*$HOME/Library/Python/3.8/bin} == "$PATH" ]] && export PATH=$PATH:$HOME/Library/Python/3.8/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/flyingice/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/flyingice/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/flyingice/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/flyingice/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
